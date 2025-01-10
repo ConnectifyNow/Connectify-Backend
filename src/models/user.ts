682d098f5e10a7e5cf57dc920028f13f2f135768
mudachii <emudahi@cs.colman.ac.il>
2025-01-11 01:42:52 +0200

    create comment model and update user model schema

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,53 +1,66 @@
-import mongoose, { Document } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IUser extends Document {
-  _id: mongoose.Types.ObjectId;
-  name: string;
+  username: string;
   email: string;
-  password?: string;
-  refreshTokens: string[];
-  type: "association" | "student" | "unknown";
-  //   image?: {
-  //     originalName?: string;
-  //     serverFilename?: string;
-  //   };
-  bio: string;
+  password: string;
+  role: number;
 }
 
-const userSchema = new mongoose.Schema<IUser>({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: false,
-  },
-  refreshTokens: {
-    type: [String],
-    required: false,
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["association", "student", "unknown"],
-  },
-  //   image: {
-  //     originalName: { type: String, required: false },
-  //     serverFilename: { type: String, required: false },
-  //   },
-  bio: {
-    type: String,
-    required: true,
-  },
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: Number, ref: "Role", required: true },
 });
 
-const UserModel = mongoose.model<IUser>("User", userSchema);
+export default mongoose.model<IUser>("User", UserSchema);
 
-export default UserModel;
+// import mongoose, { Document } from "mongoose";
+
+// export interface IUser extends Document {
+//   _id: mongoose.Types.ObjectId;
+//   name: string;
+//   email: string;
+//   password?: string;
+//   refreshTokens: string[];
+//   type: "association" | "student" | "unknown";
+//   //   image?: {
+//   //     originalName?: string;
+//   //     serverFilename?: string;
+//   //   };
+//   bio: string;
+// }
+
+// const userSchema = new mongoose.Schema<IUser>({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     lowercase: true,
+//   },
+//   password: {
+//     type: String,
+//     required: false,
+//   },
+//   refreshTokens: {
+//     type: [String],
+//     required: false,
+//   },
+//   //   image: {
+//   //     originalName: { type: String, required: false },
+//   //     serverFilename: { type: String, required: false },
+//   //   },
+//   bio: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// const UserModel = mongoose.model<IUser>("User", userSchema);
+
+// export default UserModel;
