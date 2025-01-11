a6f9d756efb16b01c5def7a5a4996d80ab4961cd
mudachii <emudahi@cs.colman.ac.il>
2025-01-11 13:16:56 +0200

    remove id field from role model schema

--- src/models/role.ts
+++ src/models/role.ts
@@ -1,12 +1,10 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IRole extends Document {
-  id: number;
   name: string;
 }
 
 const RoleSchema: Schema = new Schema({
-  id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
 });
 
