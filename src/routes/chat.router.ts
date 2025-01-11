8884fcc103d9487c27970d479b7db146e4b6c77e
hadarz-education <hadarziv2002@cs.colman.ac.il>
2025-01-11 18:34:22 +0200

    refactor: remove unused routes and controllers; add chat and message controllers

--- src/routes/chat.router.ts
+++ src/routes/chat.router.ts
@@ -1,17 +1,16 @@
 import express from "express";
 import chatController from "../controllers/chat.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import messageController from "../controllers/message.controller";
 
 const router = express.Router();
 
-router.get("/:id", authMiddleware, chatController.getChat);
-router.get("/user/userId", authMiddleware, chatController.getChatByUser);
-router.post("/", authMiddleware, chatController.create.bind(chatController));
-router.put("/:id", authMiddleware, chatController.putById.bind(chatController));
-router.delete(
-  "/:id",
+router.get("/:id?", authMiddleware, chatController.getChat);
+router.get("/user/:userId", authMiddleware, chatController.getChatsByUser);
+router.get(
+  "/:chatId/messages",
   authMiddleware,
-  chatController.deleteById.bind(chatController)
+  messageController.getMessagesByChatId
 );
 
 export default router;
