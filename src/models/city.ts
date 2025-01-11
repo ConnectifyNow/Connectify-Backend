542aded0a07e1d3561a3d43855f34c713dfd1f9d
mudachii <emudahi@cs.colman.ac.il>
2025-01-11 13:16:29 +0200

    remove id field from city model schema

--- src/models/city.ts
+++ src/models/city.ts
@@ -1,12 +1,10 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface ICity extends Document {
-  id: number;
   name: string;
 }
 
 const CitySchema: Schema = new Schema({
-  id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
 });
 
