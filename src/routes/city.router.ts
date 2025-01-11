fc4da34d37a2caffc9126a65dca5bc72cc4de103
Hila <hila.ohana1910@gmail.com>
2025-01-11 16:11:44 +0200

    upadte city route

--- src/routes/city.router.ts
+++ src/routes/city.router.ts
@@ -5,6 +5,6 @@ import authMiddleware from "../middlewares/auth.middleware";
 const router = express.Router();
 
 router.get("/", authMiddleware, cityController.getCitiesOverview);
-router.get("/:id", authMiddleware, cityController.getCitiesByUserId);
+router.get("/user/:id", authMiddleware, cityController.getCitiesByUserId);
 
 export default router;
