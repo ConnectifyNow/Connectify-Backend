3a6ba7ff83e464fd5acdd2d22559653aaa49a264
Hila <hila.ohana1910@gmail.com>
2025-01-14 21:27:01 +0200

    add file upload

---
+++ src/controllers/image.controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+const base =
+  process.env.NODE_ENV === "production"
+    ? "https://colman.ac.il/"
+    : "http://localhost:3002/";
+
+const uploadImage = async (req: Request, res: Response) => {
+  try {
+    if (!req.file) {
+      return res.status(400).send("No image file provided.");
+    }
+
+    const originalName = req.file.originalname;
+    const serverFilename = base + req.file.path;
+
+    res.status(200).send({ originalName, serverFilename });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export default {
+  uploadImage,
+};
