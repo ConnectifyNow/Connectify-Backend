fca74c19a76d0278af48e3e8a54fb02402363b85
Idan17 <idanbartov@gmail.com>
2025-01-17 16:36:41 +0200

    feat: test volunteer?

--- src/tests/volunteer.test.ts
+++ src/tests/volunteer.test.ts
@@ -4,18 +4,21 @@ import initApp from "../app"; // Assuming you have an initApp function to initia
 import { Express } from "express";
 import User from "../models/user";
 import Volunteer from "../models/volunteer";
-import { userTest, volunteerTestData } from "./mockData";
+import City from "../models/city";
+import { userTest, volunteerTestData, cityTest } from "./mockData";
 
 let app: Express;
 let accessToken: string;
 let userId: Types.ObjectId;
+let cityId: Types.ObjectId;
 
 beforeAll(async () => {
   app = await initApp();
 
-  // Drop all users and volunteers collections
+  // Drop all users, volunteers, and cities collections
   await User.deleteMany({});
   await Volunteer.deleteMany({});
+  await City.deleteMany({});
 
   // Register a user and get the access token
   const signupResponse = await request(app)
@@ -30,6 +33,10 @@ beforeAll(async () => {
   });
 
   accessToken = loginResponse.body.accessToken;
+
+  // Create a city and get its ID
+  const cityResponse = await City.create(cityTest);
+  cityId = cityResponse._id as Types.ObjectId;
 });
 
 afterAll(async () => {
@@ -45,18 +52,16 @@ describe("Volunteer Endpoints", () => {
     const response = await request(app)
       .post("/api/volunteers")
       .set("Authorization", `Bearer ${accessToken}`)
-      .send({ ...volunteerTestData.volunteer1, userId });
+      .send({ ...volunteerTestData.volunteer1, userId, city: cityId });
 
     expect(response.status).toBe(201);
-    expect(response.body.firstName).toBe(
-      volunteerTestData.volunteer1.firstName
-    );
+    expect(response.body.firstName).toBe(volunteerTestData.volunteer1.firstName);
     expect(response.body.lastName).toBe(volunteerTestData.volunteer1.lastName);
   });
 
   it("should get all volunteers", async () => {
-    await Volunteer.create({ ...volunteerTestData.volunteer1, userId });
-    await Volunteer.create({ ...volunteerTestData.volunteer2, userId });
+    await Volunteer.create({ ...volunteerTestData.volunteer1, userId, city: cityId });
+    await Volunteer.create({ ...volunteerTestData.volunteer2, userId, city: cityId });
 
     const response = await request(app)
       .get("/api/volunteers")
@@ -64,41 +69,29 @@ describe("Volunteer Endpoints", () => {
 
     expect(response.status).toBe(200);
     expect(response.body.volunteers.length).toBe(2);
-    expect(response.body.volunteers[0].firstName).toBe(
-      volunteerTestData.volunteer1.firstName
-    );
-    expect(response.body.volunteers[1].firstName).toBe(
-      volunteerTestData.volunteer2.firstName
-    );
+    expect(response.body.volunteers[0].firstName).toBe(volunteerTestData.volunteer1.firstName);
+    expect(response.body.volunteers[1].firstName).toBe(volunteerTestData.volunteer2.firstName);
   });
 
   it("should get a volunteer by ID", async () => {
-    const volunteer = await Volunteer.create({
-      ...volunteerTestData.volunteer1,
-      userId,
-    });
+    const volunteer = await Volunteer.create({ ...volunteerTestData.volunteer1, userId, city: cityId });
 
     const response = await request(app)
       .get(`/api/volunteers/${volunteer._id}`)
       .set("Authorization", `Bearer ${accessToken}`);
 
     expect(response.status).toBe(200);
-    expect(response.body.firstName).toBe(
-      volunteerTestData.volunteer1.firstName
-    );
+    expect(response.body.firstName).toBe(volunteerTestData.volunteer1.firstName);
   });
 
   it("should update a volunteer by ID", async () => {
-    const volunteer = await Volunteer.create({
-      ...volunteerTestData.volunteer1,
-      userId,
-    });
+    const volunteer = await Volunteer.create({ ...volunteerTestData.volunteer1, userId, city: cityId });
 
     const updatedVolunteerData = {
       phone: "1111111111",
       firstName: "Updated",
       lastName: "Volunteer",
-      city: "Updated City",
+      city: cityId,
       skills: [],
       about: "This is an updated volunteer",
       userId: userId,
@@ -111,14 +104,11 @@ describe("Volunteer Endpoints", () => {
 
     expect(response.status).toBe(200);
     expect(response.body.firstName).toBe(updatedVolunteerData.firstName);
-    expect(response.body.city).toBe(updatedVolunteerData.city);
+    expect(response.body.city).toBe(updatedVolunteerData.city.toString());
   });
 
   it("should delete a volunteer by ID", async () => {
-    const volunteer = await Volunteer.create({
-      ...volunteerTestData.volunteer1,
-      userId,
-    });
+    const volunteer = await Volunteer.create({ ...volunteerTestData.volunteer1, userId, city: cityId });
 
     const response = await request(app)
       .delete(`/api/volunteers/${volunteer._id}`)
@@ -139,8 +129,8 @@ describe("Volunteer Endpoints", () => {
   });
 
   it("should get volunteers by user ID", async () => {
-    await Volunteer.create({ ...volunteerTestData.volunteer1, userId });
-    await Volunteer.create({ ...volunteerTestData.volunteer2, userId });
+    await Volunteer.create({ ...volunteerTestData.volunteer1, userId, city: cityId });
+    await Volunteer.create({ ...volunteerTestData.volunteer2, userId, city: cityId });
 
     const response = await request(app)
       .get(`/api/volunteers/user/${userId}`)
@@ -148,11 +138,7 @@ describe("Volunteer Endpoints", () => {
 
     expect(response.status).toBe(200);
     expect(response.body.length).toBe(2);
-    expect(response.body[0].firstName).toBe(
-      volunteerTestData.volunteer1.firstName
-    );
-    expect(response.body[1].firstName).toBe(
-      volunteerTestData.volunteer2.firstName
-    );
+    expect(response.body[0].firstName).toBe(volunteerTestData.volunteer1.firstName);
+    expect(response.body[1].firstName).toBe(volunteerTestData.volunteer2.firstName);
   });
 });
